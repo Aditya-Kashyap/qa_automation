8d620666050eae306d6c94ebc9c17932d9a68e8e
Aditya Kashyap <adi.inhere@gmail.com>
2020-02-04 13:15:38 +0530

    Added Code

---
+++ create_check.py
@@ -0,0 +1,17 @@
+import subprocess
+from subprocess import *
+
+print("Enter the Number of Test Cases")
+num = int(input())
+
+for i in range(num):
+    run_cmd = 'xprctl create_project -f test_cases/test_case' + i
+    run = Popen(run_cmd, stdout=subprocess.PIPE, shell=True)
+    return_msg = run.communicate()[0]
+    return_code = run.returncode
+
+    if return_code == 0:
+        print("Test Case Passed")
+    else:
+        print("Test Case Failed")
+        print(return_msg)
