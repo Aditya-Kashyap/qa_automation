a1d179dc9419961f2ec3f39e95fcb97c448ddf70
Aditya Kashyap <adi.inhere@gmail.com>
2020-02-24 11:55:45 +0530

    Changed Code of Deploy

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-a = {1: 1000, 2: 2000, 3: 3000}
-for i in range(len(a)):
-    print(a[i+1])
-print(list(a.keys()))
+pipeline = {
+    "pipeline1": {
+        "flavor": "kubeflow"
+    },
+    "pipeline2": {
+        "flavor": "pyspark"
+    }
+}
+kube_pipe = None
+spark_pipe = None
+for pipe in pipeline.keys():
+    val = pipeline[pipe]
+    for pipe_comp in val.keys():
+        val1 = pipeline[pipe][pipe_comp]
+        if val1 == 'kubeflow':
+            kube_pipe = pipe
+        if val1 == 'pyspark':
+            spark_pipe = pipe
+
+print("kubeflow pipeline name: "+kube_pipe)
+print("pyspark pipeline name: "+spark_pipe)
+
+
